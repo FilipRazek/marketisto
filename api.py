8ebde2d0ed123a3659db6087cabd55ead46e53b8
Filip Razek <filip@razek.net>
2024-03-02 19:06:53 +0100

    Create api.py helper file

---
+++ api.py
@@ -0,0 +1,61 @@
+import requests
+from utils.url import build_url
+from eod import EndOfDay
+from utils.date import add_years, is_in_future
+
+api_key = "demo"
+URL_BASE = "https://api.twelvedata.com/"
+
+
+def get_earliest_timestamp(symbol):
+    url = build_url(
+        URL_BASE,
+        "earliest_timestamp",
+        {"symbol": symbol, "apikey": api_key, "interval": "1day"},
+    )
+    response = requests.get(url).json()
+    return response["datetime"]
+
+
+def get_time_series_between_dates(symbol, start_date, end_date):
+    params = {
+        "symbol": symbol,
+        "apikey": api_key,
+        "interval": "1day",
+        "start_date": start_date,
+    }
+    if end_date:
+        params["end_date"] = end_date
+    url = build_url(
+        URL_BASE,
+        "time_series",
+        params,
+    )
+
+    response = requests.get(url).json()
+    try:
+        return [
+            EndOfDay(symbol, value["datetime"], float(value["close"]))
+            for value in response["values"][::-1]
+        ]
+    except Exception as e:
+        print(symbol, start_date, end_date)
+        print(response)
+        raise e
+
+
+def get_time_series(symbol):
+    start_date = get_earliest_timestamp(symbol)
+    MAX_YEARS = 18
+    values = []
+    running = True
+    while running:
+        end_date = add_years(start_date, MAX_YEARS)
+        if is_in_future(end_date):
+            end_date = None
+            running = False
+        new_values = get_time_series_between_dates(
+            symbol, start_date, end_date)
+        values.extend(new_values)
+        start_date = end_date
+    return values
