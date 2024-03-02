e201dd21ef4def5bbb92a10c87bf3ed1a1006a4d
Filip Razek <filip@razek.net>
2024-03-02 19:07:12 +0100

    Refactor main.py to calculate daily variations

--- main.py
+++ main.py
@@ -1,77 +1,25 @@
-import requests
-import matplotlib.pyplot as plt
+from api import get_time_series
 
-from utils.date import add_years, is_in_future
-from utils.url import build_url
-from eod import EndOfDay
 
 symbol = "AAPL"
-api_key = "demo"
-
-URL_BASE = "https://api.twelvedata.com/"
 
 
-def get_earliest_timestamp(symbol):
-    url = build_url(
-        URL_BASE,
-        "earliest_timestamp",
-        {"symbol": symbol, "apikey": api_key, "interval": "1day"},
-    )
-    response = requests.get(url).json()
-    return response["datetime"]
+def get_daily_variations(time_series):
+    variations = []
+    for i in range(1, len(time_series)):
+        current = time_series[i].value
+        previous = time_series[i - 1].value
+        variations.append((current - previous) / previous)
+    return variations
 
 
-def get_time_series_between_dates(symbol, start_date, end_date):
-    params = {
-        "symbol": symbol,
-        "apikey": api_key,
-        "interval": "1day",
-        "start_date": start_date,
-    }
-    if end_date:
-        params["end_date"] = end_date
-    url = build_url(
-        URL_BASE,
-        "time_series",
-        params,
-    )
-
-    response = requests.get(url).json()
-    try:
-        return [
-            EndOfDay(symbol, value["datetime"], value["close"])
-            for value in response["values"][::-1]
-        ]
-    except Exception as e:
-        print(symbol, start_date, end_date)
-        print(response)
-        raise e
+def get_expected_daily_variation(symbol):
+    time_series = get_time_series(symbol)
+    daily_variations = get_daily_variations(time_series)
+    total_growth = 1
+    for variation in daily_variations:
+        total_growth *= 1 + variation
+    return total_growth ** (1 / len(daily_variations)) - 1
 
 
-def get_time_series(symbol):
-    start_date = get_earliest_timestamp(symbol)
-    MAX_YEARS = 18
-    values = []
-    running = True
-    while running:
-        end_date = add_years(start_date, MAX_YEARS)
-        if is_in_future(end_date):
-            end_date = None
-            running = False
-        new_values = get_time_series_between_dates(
-            symbol, start_date, end_date)
-        values.extend(new_values)
-        start_date = end_date
-    return values
-
-
-time_series = get_time_series(symbol)
-
-x = []
-y = []
-for eod in time_series:
-    x.append(eod.date)
-    y.append(eod.value)
-
-plt.plot(x, y)
-plt.show()
+print(get_expected_daily_variation(symbol))
