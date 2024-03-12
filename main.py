21e247d698bbcb602ccd1ae18a50dce1d8de458e
Filip Razek <filip@razek.net>
2024-03-12 22:15:26 +0100

    :recycle: Move get_summary to utils

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 from prettytable import PrettyTable
-from api import get_time_series, get_earliest_timestamp
-from calc import get_expected_yearly_variation
+
+from util import get_summary
 
 symbol = "AAPL"
-
-
-def get_summary(symbol):
-    start_date = get_earliest_timestamp(symbol)
-    time_series = get_time_series(symbol, start_date)
-    yearly_variation = f"{100 * get_expected_yearly_variation(time_series):.2f}%"
-    return [
-        symbol,
-        start_date,
-        f"${time_series[0].value}",
-        f"${time_series[-1].value}",
-        yearly_variation,
-    ]
 
 
 def build_summary_table(symbols):
--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+from api import get_time_series, get_earliest_timestamp
+
+
 def get_daily_variations(time_series):
     variations = []
     for i in range(1, len(time_series)):
@@ -13,3 +16,16 @@ def get_expected_yearly_variation(time_series):
     for variation in daily_variations:
         total_growth *= 1 + variation
     return total_growth ** (365 / len(daily_variations)) - 1
+
+
+def get_summary(symbol):
+    start_date = get_earliest_timestamp(symbol)
+    time_series = get_time_series(symbol, start_date)
+    yearly_variation = f"{100 * get_expected_yearly_variation(time_series):.2f}%"
+    return [
+        symbol,
+        start_date,
+        f"${time_series[0].value}",
+        f"${time_series[-1].value}",
+        yearly_variation,
+    ]
