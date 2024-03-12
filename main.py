2db9e5b9b9f4fccb7b0f7aeda6d8f1dedfe5a9d9
Filip Razek <filip@razek.net>
2024-03-12 22:13:30 +0100

    :sparkles: Add summary table

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from prettytable import PrettyTable
 from api import get_time_series, get_earliest_timestamp
 from calc import get_expected_yearly_variation
 
@@ -8,13 +9,26 @@ def get_summary(symbol):
     start_date = get_earliest_timestamp(symbol)
     time_series = get_time_series(symbol, start_date)
     yearly_variation = f"{100 * get_expected_yearly_variation(time_series):.2f}%"
-    return (
+    return [
         symbol,
         start_date,
         f"${time_series[0].value}",
         f"${time_series[-1].value}",
         yearly_variation,
-    )
+    ]
 
 
-print(get_summary(symbol))
+def build_summary_table(symbols):
+    table = PrettyTable()
+    table.field_names = [
+        "Symbol",
+        "Start Date",
+        "Start Price",
+        "End Price",
+        "Yearly Variation",
+    ]
+    table.add_rows([get_summary(symbol) for symbol in symbols])
+    return table
+
+
+print(build_summary_table([symbol, symbol, symbol]))
